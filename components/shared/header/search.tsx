b1b2a79e46f691ee4e62c4be5d9b77ce74d659b4
Temesgen Birechis <birechis@gmail.com>
2025-08-06 09:36:56 +0300

    edited header component

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -1,6 +1,5 @@
 import { SearchIcon } from 'lucide-react';
 import { Input } from '@/components/ui/input';
-
 import {
   Select,
   SelectContent,
@@ -8,14 +7,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-
 import { APP_NAME } from '@/lib/constants';
 
 const categories = ['men', 'women', 'kids', 'accessories'];
 
 export default async function Search() {
   return (
-    <form action="/search" method="GET" className="flex items-streach h-10">
+    <form action="/search" method="GET" className="flex items-stretch ">
       <Select name="category">
         <SelectTrigger className="w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r rounded-r-none rounded-l-md">
           <SelectValue placeholder="All" />
@@ -33,13 +31,12 @@ export default async function Search() {
         name="q"
         type="search"
         placeholder={`Search site ${APP_NAME}`}
-        className="flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full"
+        className="flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base"
       />
       <button
         type="submit"
-        className="bg-primary text-primary-foreground rounded-s-none rounded-e-md h-full px-3 py-2 "
+        className="bg-primary text-primary-foreground rounded-s-none rounded-e-md px-3 py-1"
       >
-        {' '}
         <SearchIcon className="w-6 h-6" />
       </button>
     </form>
